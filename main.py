9d1191e9f0af611ba7139a7eb461f0321357e92a
Alejandro <alexkid20025@gmail.com>
2025-07-11 21:23:09 +0200

    added json as a database, changed roots, get and delete for a new one

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 import json
-from typing import Union
-
 from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
-from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+root_template = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'BSCToDoList'))
+root_database = os.path.join(os.path.dirname(__file__), 'database.json')
+templates=Jinja2Templates(directory=root_template)
 
+# Get item from database.json
+# Return the item to todolist.html
 @app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+async def root(request: Request):
+    with open(root_database) as f:
+        data = json.load(f)
+    return templates.TemplateResponse('todolist.html',{"request":request,"tododict":data})
 
-
-# Get item
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-# Edit item
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+# Delete item from the list
+@app.get("/delete/{id}")
+async def delete_todo(request:Request,id:str):
+    with open('database.json') as f:
+        data=json.load(f)
+    del data[id]
+    with open('database.json','w') as f:
+        json.dump(data,f)
+    return RedirectResponse("/",303)
 
 # Add new item to the list
 @app.post("/add")
